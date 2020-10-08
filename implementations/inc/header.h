d7743d87615946eb0e87881451f80e1d9055bdca
99002769 <72495916+99002769@users.noreply.github.com>
2020-10-08 06:42:03 +0530

    Update header.h

--- implementations/inc/header.h
+++ implementations/inc/header.h
@@ -1,1 +1,18 @@
 
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+ 
+
+ 
+
+
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+void calculator_operations();
